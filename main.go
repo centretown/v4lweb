c0d1acabd8b5875c06722e6d0c39a8534fbaf7d3
Dave Marsh <davemarsh77@gmail.com>
2024-11-18 17:45:56 -0500

    camera works on pc

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
 	"v4lweb/camera"
 	"v4lweb/web"
 )
@@ -20,10 +25,69 @@ func main() {
 		Height:     1080,
 		FPS:        30,
 	}
+
+	cfg, err := LoadConfig("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := &http.ServeMux{}
+	httpServer := &http.Server{
+		Handler:      mux,
+		Addr:         cfg.HttpUrl,
+		ReadTimeout:  0,
+		WriteTimeout: 0,
+	}
+
 	var indicator = &Indicator{}
 	srv, err := web.NewCameraServer(0, config, indicator)
 	if err != nil {
 		log.Fatal(err)
 	}
-	go srv.Serve()
+	web.ServeCamera(mux, srv)
+
+	httpErr := make(chan error, 1)
+	go func() {
+		httpErr <- httpServer.ListenAndServe()
+	}()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+	select {
+	case err := <-httpErr:
+		log.Printf("failed to serve http: %v", err)
+	case sig := <-sigs:
+		log.Printf("terminating: %v", sig)
+	}
+
+}
+
+type Config struct {
+	HttpUrl string
+	Camera  *camera.VideoConfig
+}
+
+func LoadConfig(filename string) (cfg *Config, err error) {
+	cfg = &Config{}
+	var f *os.File
+	f, err = os.Open(filename)
+	if err != nil {
+		log.Println("config.Load Open", err)
+		return
+	}
+	defer f.Close()
+	var buf []byte
+	buf, err = io.ReadAll(f)
+	if err != nil {
+		log.Println("config.Load ReadAll", err)
+		return
+	}
+	err = json.Unmarshal(buf, cfg)
+	if err != nil {
+		log.Println("config.Load Unmarshal", err)
+		return
+	}
+
+	log.Println(cfg.HttpUrl, cfg.Camera.Codec)
+	return
 }
--- web/camera_handlers.go
+++ web/camera_handlers.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"log"
+	"net/http"
 	"v4lweb/camera"
 )
 
@@ -19,4 +21,21 @@ func NewCameraServer(id int, vcfg *camera.VideoConfig,
 	cameraServer = camera.NewVideoServer(id, source, vcfg, indicator)
 	err = cameraServer.Open()
 	return
+}
+
+func ServeCamera(mux *http.ServeMux, camServer *camera.Server) {
+	log.Println("serveCamera", camServer.Url())
+	mux.Handle(camServer.Url(), camServer.Stream())
+	// source := camServer.Source
+	// webcam, isWebcam := source.(*camera.Webcam)
+	// if isWebcam {
+	// 	ctll := NewControlList(mux, webcam, 0, data.WebcamHandlers)
+	// 	data.mux.HandleFunc("/resetcontrols",
+	// 		func(w http.ResponseWriter, r *http.Request) {
+	// 			ctll.ResetControls()
+	// 		})
+	// }
+
+	go camServer.Serve()
+	log.Printf("Serving %s\n", camServer.Url())
 }
